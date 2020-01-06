8eb80d72a43e408036b1bdf42e94df14489ac2da
Dima Vidieiev <dimich2993@gmail.com>
2020-01-06 22:15:56 +0200

    Implemented sprite generation function

--- Assets/LevelInit.cs
+++ Assets/LevelInit.cs
@@ -4,40 +4,24 @@ using UnityEngine;
 
 public class LevelInit : MonoBehaviour
 {
-    public GameObject obj;
+    public Vector2Int fieldDimensions = new Vector2Int(10, 10);
+    public int spriteSize = 32;
+    public Vector2 pivot = new Vector2(0.5f, 0.5f);
+    public GameObject[] spriteObjects;
+    public Random rnd = new Random();
 
     // Start is called before the first frame update
     void Start()
     {
         try
         {
-            this.obj = new GameObject("Level");
-            var spriteSize = 32;
-            var pivot = new Vector2(0.5f, 0.5f);
+            this.spriteObjects = new GameObject[fieldDimensions.x * fieldDimensions.y];
 
-            var texture2D = Resources.Load<Texture2D>("Textures");
+            var sprites = LoadSprites("Textures");
 
-            if (texture2D == null) {
-                throw new System.Exception("Texture not loaded");
-            }
-            
-            var sprites = new Sprite[48];
+            FillFieldWithSprite(sprites[0]);
 
-            for (int i = 0, current = 0; i < 6; ++i) {
-                for (int j = 0; j < 8; ++j, ++current)
-                {
-                    sprites[current] = Sprite.Create(
-                        texture2D,
-                        new Rect(spriteSize * j, spriteSize * i, spriteSize, spriteSize),
-                        pivot
-                    );
-                }
-            }
-
-            var rend = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
-
-            rend.transform.position = Vector3.zero;
-            rend.sprite = sprites[0];
+            StartCoroutine(UpdateSpriteObject());
 
             print("Success!!!");
         }
@@ -50,6 +34,53 @@ public class LevelInit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public IEnumerator UpdateSpriteObject() {
+        while (true)
+        {
+            print("Tick");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    public Sprite[] LoadSprites(string path) {
+        var texture2D = Resources.Load<Texture2D>(path);
+
+        if (texture2D == null) {
+            throw new System.Exception("Texture not loaded");
+        }
+
+        var sprites = new Sprite[48];
+
+        for (int i = 0, current = 0; i < 6; ++i) {
+            for (int j = 0; j < 8; ++j, ++current)
+            {
+                sprites[current] = Sprite.Create(
+                    texture2D,
+                    new Rect(spriteSize * j, spriteSize * i, spriteSize, spriteSize),
+                    pivot
+                );
+            }
+        }
+
+        return sprites;
+    }
+
+    public void FillFieldWithSprite(Sprite initialSprite) {
+        for (int y = 0, c = 0; y < fieldDimensions.y; ++y)
+        {
+            for (int x = 0; x < fieldDimensions.x; ++x, ++c)
+            {
+                var obj = new GameObject($"{y}*{x}");
+                var rend = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+
+                rend.sprite = initialSprite;
+                rend.transform.localScale = new Vector3(3f, 3f, 3f);
+                rend.transform.position = new Vector3(x - fieldDimensions.x / 2, y - fieldDimensions.y / 2, Vector3.zero.z);
+                this.spriteObjects[c] = obj;
+            }
+        }
     }
 }
